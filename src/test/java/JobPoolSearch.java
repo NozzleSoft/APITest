6ea06f9fc011d339c7c7ad2c3e6068ade608fea3
Berkay Yıldız <berkay.yildiz@nozzlesoft.com>
2022-07-01 22:40:51 +0300

    commit

--- src/test/java/JobPoolSearch.java
+++ src/test/java/JobPoolSearch.java
@@ -1,11 +1,11 @@
+import io.restassured.RestAssured;
 import org.testng.annotations.Test;
 
 public class JobPoolSearch {
-
-
     @Test
     void  Test(){
-        System.out.println("SAD");
+       RestAssured.given().post("https://apistage.nozzlesoft.com/api/Account/Login");
+
     }
 
 }
